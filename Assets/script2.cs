b2b709b481b9eed33cfbb3b6f686f46e5ec43f3c
fedevitacca <vitaccafede@gmail.com>
2025-06-18 15:31:49 -0300

    array

---
+++ Assets/script2.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class script2 : MonoBehaviour
+{
+
+    public int[] edades;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //escribir el primer punto del array 
+        Debug.Log(edades[0]);
+
+        //escribir el largo del array 
+        Debug.Log(edades.Length);
+
+        // escribir el ultimo elemento del array
+        Debug.Log(edades[edades.Length - 1]);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
