5e5a75e624b37a56b1c686e16a367f6fabb0ae5c
fedevitacca <vitaccafede@gmail.com>
2025-06-18 16:00:55 -0300

    cubos hechos

---
+++ Assets/scripts/CubosManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubosManager : MonoBehaviour
+{
+
+    public CubosScript[] cubos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cubos = FindObjectsOfType<CubosScript>();
+        for (int i = 0; i < cubos.Length; i++)
+        {
+            if (!cubos[i].isPersistent)
+            {
+                Destroy(cubos[i].gameObject);
+            }
+        }
+
+        //posicionar todos los cubos en el cero del espacio
+        for (int i = 0; i< cubos.Length; i++)
+        {
+            cubos[i].transform.position = new Vector3(0, 0, 0);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
